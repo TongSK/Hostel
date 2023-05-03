339f06372815c8aca553e53b8350805f2338fa07
SK Tong <shengkit122@gmail.com>
2023-05-03 14:52:56 +0800

    Add room child class

---
+++ src/main/java/com/mycompany/hostelmanagement/room.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hostelmanagement;
+
+/**
+ *
+ * @author Asus
+ */
+public class room extends hostel {
+
+    public room(String roomID, String roomName, String roomType, String desc, double price, String avail, String owner) {
+        super(roomID, roomName, roomType, desc, price, avail, owner);
+    }
+    
+    
+    
+    
+    
+}
