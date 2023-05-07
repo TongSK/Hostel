0022cca99084484e89ed1be1c7341a06bb07d3c9
SK Tong <shengkit122@gmail.com>
2023-05-07 17:41:56 +0800

    Update room child class

--- src/main/java/com/mycompany/hostelmanagement/room.java
+++ src/main/java/com/mycompany/hostelmanagement/room.java
@@ -9,13 +9,19 @@ package com.mycompany.hostelmanagement;
  * @author Asus
  */
 public class room extends hostel {
-
+    private hostel roomData;
+    
     public room(String roomID, String roomName, String roomType, String desc, double price, String avail, String owner) {
         super(roomID, roomName, roomType, desc, price, avail, owner);
     }
     
+     public void setRoomData(hostel roomData){
+        this.roomData = roomData;
+    }
     
-    
+    public hostel getRoomData(){
+        return this.roomData;
+    }
     
     
 }
