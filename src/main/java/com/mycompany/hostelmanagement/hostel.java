4b7ebeed6564ba51100ed627413c17ed2cebf55b
SK Tong <shengkit122@gmail.com>
2023-05-07 17:41:11 +0800

    Update hostel Parent class

--- src/main/java/com/mycompany/hostelmanagement/hostel.java
+++ src/main/java/com/mycompany/hostelmanagement/hostel.java
@@ -29,6 +29,7 @@ public class hostel {
     protected double price;
     protected String avail;
     protected String owner;
+    
 
     public String getRoomID() {
         return roomID;
@@ -57,7 +58,9 @@ public class hostel {
     public String getOwner() {
         return owner;
     }
-
+    
+ 
+    
     public hostel(String roomID, String roomName, String roomType, String desc, double price, String avail, String owner) {
         this.roomID = roomID;
         this.roomName = roomName;
@@ -66,58 +69,6 @@ public class hostel {
         this.price = price;
         this.avail = avail;
         this.owner = owner;
-    }
-    
-    
-    public ArrayList<String> mainFunc(String fileName, String func){
-        ArrayList<String> tmp = new ArrayList<>();
-        try {
-            String[] roomls;
-            String line ;
-            File f = new File(fileName);
-            Scanner s = new Scanner(f);
-                
-            while(s.hasNextLine()){
-                if((line = s.nextLine()) != null){
-                    roomls = line.split(",");
-                    switch (func) {
-                        case "r" -> tmp.add(line);
-                        case "w" -> {
-                            if(getRoomID().equals(roomls[0])){
-                                tmp.add(getRoomID()+","+getRoomName()+","+getRoomType()+","+getDesc()+","+getPrice()+","+getAvail()+","+getOwner());
-                            }else{
-                                tmp.add(line);
-                            }
-                        }
-                        case "a" -> {
-                            if(getRoomID().equals(roomls[0])){
-                                return tmp;
-                            }else{
-                                tmp.add(getRoomID()+","+getRoomName()+","+getRoomType()+","+getDesc()+","+getPrice()+","+getAvail()+","+getOwner());
-                                return tmp;
-                            }
-                           
-                        }
-                        
-                        case "d" -> {
-                            tmp.add(line);
-                            
-                        }
-                        default -> tmp = null;
-                    }
-            
-                }
-            }
-            
-            
-            
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(hostel.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(hostel.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return tmp;
     }
     
     public void displyData(ArrayList<String> tmp , JTable tblName){
